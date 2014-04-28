22a114aeb02efcbe5f6267d0b3f5e773de8615b5
Michael Raimondi <zeno@pobox.com>
2014-04-27 20:43:02 -0400

    Extract pomodoro notification to service object

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -18,24 +18,11 @@ class Pomodoro < ActiveRecord::Base
 
   def add_end
     if self.update(end: DateTime.now)
-      send_notification!
+      notifier = PomodoroNotifier.new(self)
+      notifier.send_notification!
     else
       # Something has gone seriously wrong, just destroy the invalid pomodoro
       self.destroy
     end
-  end
-
-  private
-
-  def send_notification!
-    account_sid = 'AC0392a7666e358b617c37331aa14b0b3c'
-    auth_token = '50a8977da9b70fdfcfeec6524561103b'
-
-    client = Twilio::REST::Client.new account_sid, auth_token
-    client.account.messages.create(
-      from: '+16175003913',
-      to: user.phone_number,
-      body: 'Pomodoro complete!'
-    )
   end
 end
---
+++ app/models/pomodoro_notifier.rb
@@ -0,0 +1,17 @@
+class PomodoroNotifier
+  def initialize(pomodoro)
+    @pomodoro = pomodoro
+  end
+
+  def send_notification!
+    account_sid = 'AC0392a7666e358b617c37331aa14b0b3c'
+    auth_token = '50a8977da9b70fdfcfeec6524561103b'
+
+    client = Twilio::REST::Client.new account_sid, auth_token
+    client.account.messages.create(
+      from: '+16175003913',
+      to: @pomodoro.user.phone_number,
+      body: 'Pomodoro complete!'
+    )
+  end
+end
