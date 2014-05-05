e1e59521a837b207e40c237d0fdb0ad8c6e12299
Michael Raimondi <zeno@pobox.com>
2014-05-04 22:41:23 -0400

    Hide API keys

--- app/models/pomodoro_notifier.rb
+++ app/models/pomodoro_notifier.rb
@@ -6,10 +6,9 @@ class PomodoroNotifier
 
   def send_notification!
     if @pomodoro.update(end: DateTime.now)
-      account_sid = 'AC0392a7666e358b617c37331aa14b0b3c'
-      auth_token = '50a8977da9b70fdfcfeec6524561103b'
+      client = Twilio::REST::Client.new(Rails.application.secrets.twilio_account_sid,
+                                        Rails.application.secrets.twilio_auth_token)
 
-      client = Twilio::REST::Client.new account_sid, auth_token
       client.account.messages.create(
         from: '+16175003913',
         to: @pomodoro.user.phone_number,
