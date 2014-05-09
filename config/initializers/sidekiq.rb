3c0808f1e1ce27ed591cee381e2ccb587ff9e080
Michael Raimondi <zeno@pobox.com>
2014-05-08 21:19:49 -0400

    Configure Redis connection for deployment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'timevault' }
+  config.redis = { url: Rails.application.secrets.redis_url, namespace: 'timevault' }
   config.poll_interval = 5
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'timevault' }
+  config.redis = { url: Rails.application.secrets.redis_url, namespace: 'timevault' }
 end
